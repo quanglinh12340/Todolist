d685e1c04c936dd28558ba5e31be9239efecd7dd
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-14 16:52:11 +0700

    Todolist

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./Navbar.module.scss";
+
+const cx = classNames.bind(styles);
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
